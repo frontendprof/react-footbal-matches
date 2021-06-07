cef45a6a4d1fa4974491912ec7130a559688fb42
frontendprof <https://github.com/frontendprof>
2021-06-07 15:44:15 +0500

    creating footer and logo

---
+++ src/utils/tools.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import {Link} from "react-router-dom";
+
+import mCityLogo from "../Resources/images/logos/manchester_city_logo.png";
+
+export const CityLogo=(props)=>{
+    const templates=<div className="img_cover"
+        style={{ 
+            width:props.width,
+            height:props.height,
+            background:`url(${mCityLogo}) no-repeat` 
+        }}
+    ></div>
+
+    if(props.link){
+        return(<Link  className="link_logo" to={props.linkTo}>
+                {templates}
+            </Link>)
+    }else{
+        return templates;
+    }
+}
