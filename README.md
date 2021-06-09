24897a01b7979ca209cecb4edba6bd9ad4fe6ce8
frontendprof <https://github.com/frontendprof>
2021-06-09 13:54:33 +0500

    signin component with formik

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 ### Initial setup
 
+1. Removing unnecessary files
+2. Cleanup
+3. Wiring it up to github account
+
 ### Components
+
+1. Home component
+2. Header component
+3. Footer component
+4. Signin component
--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import {Switch,Route,BrowserRouter} from "react-router-dom"
 import Header from "./components/Header";
 import Footer from './components/Footer'
 import Home from './components/Home'
+import Signin from './components/Signin';
 
 const Routes = () => {
   return (
     <BrowserRouter>
     <Header />
       <Switch>
+        <Route path="/sign_in" exact component={Signin}/>
         <Route path="/" exact component={Home}/>
       </Switch>
       <Footer />
